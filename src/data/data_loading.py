b8b866c59369f1b7e8343a9121bdd66f82b9a697
sebocam <cambalsebo@gmail.com>
2024-11-04 13:14:33 +0100

    Update data_loading.py

--- src/data/data_loading.py
+++ src/data/data_loading.py
@@ -1,28 +1,20 @@
 import numpy as np
 import logging
 import pandas as pd
-import os
+import concurrent.futures
 from .model_actions.NN_module import PredictionModule
 
 
 class DataModule:
     def __init__(self):
-        print(os.getcwd())
-        self.dataPath = '../data/processed/EEA-SK-Ba-trend.csv'
+        # logging setup
         self.logger = logging.getLogger(__name__)
+
+        # data setup
+        self.dataPath = '../data/processed/EEA-SK-Ba-trend.csv'
         self.idx = 48
-        self.df = pd.read_csv(self.dataPath)
-        self.pred_df = pd.DataFrame(
-            np.nan,
-            index=range(self.idx),
-            columns=[
-                'DatetimeBegin',
-                'PM10 Concentration',
-                'PM2.5 Concentration',
-                'NO2 Concentration'
-            ]
-        )
         self.obsolete = True
+        self.df = pd.read_csv(self.dataPath)
         self.data = {
             'dates': self.df['DatetimeBegin'].tolist()[:self.idx + 1],
             'PM10': self.df['PM10 Concentration'].tolist()[:self.idx],
@@ -32,44 +24,49 @@ class DataModule:
         self.data['PM10pred'] = [None] * self.idx
         self.data['PM25pred'] = [None] * self.idx
         self.data['NO2pred'] = [None] * self.idx
+
+        # prediction setup
         self.driftModule = DriftModule()
-        self.predictionModeule = PredictionModule()
-        self.nextCallCaount = 0
+        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
+        self.predictionModule = PredictionModule()
+        self.nextCallCount = 0
 
     def nextData(self) -> None:
-        self.obsolete = False
         self.data['dates'] = self.df['DatetimeBegin'].tolist()[:self.idx + 1]
         self.data['PM10'] = self.df['PM10 Concentration'].tolist()[:self.idx]
         self.data['PM25'] = self.df['PM2.5 Concentration'].tolist()[:self.idx]
         self.data['NO2'] = self.df['NO2 Concentration'].tolist()[:self.idx]
 
-        self.savePredictions(
-            self.predictionModeule.predict(self.prepareDataForPrediction())
-        )
+        future = self.executor.submit(self.runPrediction)
+        future.add_done_callback(self.savePredictionsCallback)
 
-        self.data['PM10pred'] = self.pred_df['PM10 Concentration'].tolist()
-        self.data['PM25pred'] = self.pred_df['PM2.5 Concentration'].tolist()
-        self.data['NO2pred'] = self.pred_df['NO2 Concentration'].tolist()
         self.logger.info('Data updated')
+        self.obsolete = False
 
-        self.nextCallCaount += 1
-        if self.nextCallCaount % 16 == 0:
-            self.driftModule.detectDrift(self.data)
-            self.nextCallCaount = 0
+        # self.nextCallCaount += 1
+        # if self.nextCallCaount % 16 == 0:
+        #     self.driftModule.detectDrift(self.data)
+        #     self.nextCallCount = 0
+
+    def runPrediction(self):
+        return self.predictionModule.predict(self.prepareDataForPrediction())
+
+    def savePredictionsCallback(self, future):
+        try:
+            predictions = future.result()
+            self.savePredictions(predictions)
+            self.data['PM10pred'].append(self.df.loc[self.idx + 1, 'PM10 pred'])
+            self.data['PM25pred'].append(self.df.loc[self.idx + 1, 'PM25 pred'])
+            self.data['NO2pred'].append(self.df.loc[self.idx + 1, 'NO2 pred'])
+            self.obsolete = False
+            self.logger.info('Predictions updated')
+        except Exception as e:
+            self.logger.error(f"Prediction failed: {str(e)}")
 
     def savePredictions(self, data: dict) -> None:
-        date = self.df['DatetimeBegin'].iloc[self.idx + 1]
-        newRow = pd.DataFrame({
-            'DatetimeBegin': [date],
-            'PM10 Concentration': [data['PM10']],
-            'PM2.5 Concentration': [data['PM25']],
-            'NO2 Concentration': [data['NO2']]
-        })
-        self.pred_df = pd.concat(
-            [self.pred_df, newRow],
-            ignore_index=True
-        )
-        self.obsolete = True
+        self.df.loc[self.idx + 1, 'PM10 pred'] = float(data['PM10'])
+        self.df.loc[self.idx + 1, 'PM25 pred'] = float(data['PM25'])
+        self.df.loc[self.idx + 1, 'NO2 pred'] = float(data['NO2'])
 
     def prepareDataForPrediction(self) -> np.ndarray:
         return np.array([
